f5cd5b68f6e096da917ae5c80fd203633a11b19e
shivani818 <shivani.patil.3591@gmail.com>
2023-10-25 19:10:24 +0530

    nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,15 @@
 
   </div>
   <div class="w-full h-[88px] bg-white shadow md:pl-20 flex items-center flex-row justify-between">
-    <img class="w-6 h-6 relative" src="../assets/images/Group_3.svg" />
+    <!-- <img class="w-6 h-6 relative" src="../assets/images/Group_3.svg" /> -->
+    <button class="relative">
+    <div class=" flex flex-col justify-between w-[20px] h-[20px]">
+      <div class="bg-[#770015] h-[2px] w-1/2 rounded transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-right delay-75 group-focus:-translate-y-[1px]"></div>
+      <div class="bg-[#770015] h-[1px] rounded"></div>
+      <div class="bg-[#770015] h-[2px] w-1/2 rounded self-end transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-left delay-75 group-focus:translate-y-[1px]"></div>
+    </div>
+    </button>
+    
     <img class="w-20 h-20 relative" src="../assets/images/elogo.png" />
     <div class="flex items-center md:pr-20 flex-row md:space-x-2">
       <img class="w-6 h-6 relative" src="../assets/images/Frame_1.svg" />
@@ -31,6 +39,8 @@
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Navbar',
+  
+
 
 }
 </script>
